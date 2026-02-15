e35cb2ef969af002fe5cc8da4de10bfe9aa0354f
aarafathh <aarafathh@gmail.com>
2026-02-15 14:25:22 +0600

    Add image uploading functionality

--- SwiftUIFirebaseChat/LoginView.swift
+++ SwiftUIFirebaseChat/LoginView.swift
@@ -8,15 +8,18 @@
 import SwiftUI
 import Firebase
 import FirebaseAuth
+import FirebaseStorage
 
 class FirebaseManager: NSObject {
     let auth: Auth
+    let storage: Storage
     
     static let shared = FirebaseManager()
     
     override init() {
         FirebaseApp.configure()
         self.auth = Auth.auth()
+        self.storage = Storage.storage()
         super.init()
     }
 }
@@ -25,6 +28,8 @@ struct LoginView: View {
     @State var isLoginMode = false
     @State var email = ""
     @State var password = ""
+    
+    @State var shouldShowImagePicker = false
     
     var body: some View {
         NavigationView {
@@ -40,11 +45,22 @@ struct LoginView: View {
                     
                     if !isLoginMode {
                         Button {
-                            
+                            shouldShowImagePicker.toggle()
                         } label: {
-                            Image(systemName: "person.fill")
-                                .font(.system(size: 64))
-                                .padding()
+                            VStack {
+                                if let image = self.image {
+                                    Image(uiImage: image)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 128, height: 128)
+                                        .cornerRadius(64)
+                                } else {
+                                    Image(systemName: "person.fill")
+                                        .font(.system(size: 64))
+                                        .padding()
+                                        .foregroundColor(Color(.label))
+                                }
+                            }
                         }
                     }
 
@@ -79,7 +95,12 @@ struct LoginView: View {
                 .ignoresSafeArea())
         }
         .navigationViewStyle(StackNavigationViewStyle())
+        .fullScreenCover(isPresented: $shouldShowImagePicker, onDismiss: nil) {
+            ImagePicker(image: $image)
+        }
     }
+    
+    @State var image: UIImage?
     
     private func handleAction() {
         if isLoginMode {
@@ -114,6 +135,31 @@ struct LoginView: View {
             }
             print("[AAAA] Successfull : \(result?.user.uid)")
             self.loginStatusMessage = "Successfully created user"
+            self.persistImageToStorage()
+        }
+    }
+    
+    private func persistImageToStorage() {
+        let filename = UUID().uuidString
+        guard let uid = FirebaseManager.shared.auth.currentUser?.uid else { return }
+        
+        let ref = FirebaseManager.shared.storage.reference(withPath: uid)
+        
+        guard let imageData = self.image?.jpegData(compressionQuality: 0.5) else { return }
+        
+        ref.putData(imageData, metadata: nil) { metadata, err in
+            if let err = err {
+                self.loginStatusMessage = "Failed to push image to storage \(err)"
+                return
+            }
+            
+            ref.downloadURL { url, err in
+                if let err = err {
+                    self.loginStatusMessage = "Failed to retrieve downloadurl \(err)"
+                    return
+                }
+                self.loginStatusMessage = "Successfully sotred image with url = \(url?.absoluteString)"
+            }
         }
     }
 }
